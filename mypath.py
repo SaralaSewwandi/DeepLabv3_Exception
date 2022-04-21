33f98b5f421cc38d908c58a54ed438f4470b2ac5
Selventhiran Rengaraj <selventhiran@multicorewareinc.com>
2022-04-21 15:24:06 +0530

    deeplabv3 exception backbone - baseline validation, onnx export and onnx validation done

---
+++ mypath.py
@@ -0,0 +1,14 @@
+class Path(object):
+    @staticmethod
+    def db_root_dir(dataset):
+        if dataset == 'pascal':
+            return '/home/bmw/sarala/Datasets/VOCdevkit/VOC2012/'  # folder that contains VOCdevkit/.
+        elif dataset == 'sbd':
+            return '/home/dataset/pascal_voc/benchmark_RELEASE/'  # folder that contains dataset/.
+        elif dataset == 'cityscapes':
+            return '/path/to/datasets/cityscapes/'     # foler that contains leftImg8bit/
+        elif dataset == 'coco':
+            return '/path/to/datasets/coco/'
+        else:
+            print('Dataset {} not available.'.format(dataset))
+            raise NotImplementedError
